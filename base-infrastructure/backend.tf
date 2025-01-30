3c13d3d76c45b20ac00a64bdfb62666692f46c42
mikhailouski <yurimikhailouski@gmail.com>
2025-01-30 03:38:16 +0300

    Added backend.tf, minor changes to workflow

---
+++ base-infrastructure/backend.tf
@@ -0,0 +1,10 @@
+# Used after initial creation of S3 Bucket
+terraform {
+  backend "s3" {
+    bucket         = "yuri-tf-state-bucket"
+    key            = "base-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "yuri-terraform-locks"
+    encrypt        = true
+  }
+}
